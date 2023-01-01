b8b722bb61f1f2e91361372330f494cd8ee086b2
josephamcdonald <github@josephamcdonald.com>
2023-01-01 13:57:27 -0600

    Update Copyright to 2023.

--- newzy/src/main/java/com/josephamcdonald/android/newzy/NewzyHolder.java
+++ newzy/src/main/java/com/josephamcdonald/android/newzy/NewzyHolder.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019-2022. josephamcdonald - All Rights Reserved
+ * Copyright (c) 2019-2023. josephamcdonald - All Rights Reserved
  *  Unauthorized copying of this file, via any medium is strictly prohibited
  *  Proprietary and confidential
  *  Written by josephamcdonald
