2c9d2d4661c82310edc89d4022af3c5d4804710e
josephamcdonald <github@josephamcdonald.com>
2023-02-10 22:20:35 -0600

    Add Glide error drawable.

--- newzy/src/main/java/com/josephamcdonald/android/newzy/NewzyHolder.java
+++ newzy/src/main/java/com/josephamcdonald/android/newzy/NewzyHolder.java
@@ -82,7 +82,9 @@ public class NewzyHolder extends RecyclerView.ViewHolder implements View.OnClick
                     .load(currentNewzy.getNewzyImage())
                     .override(IMAGE_TARGET_WIDTH, IMAGE_TARGET_HEIGHT)
                     .centerCrop()
+                    .error(R.drawable.newzy_banner)
                     .into(newzyImageView);
+
         }
     }
 
